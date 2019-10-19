88c93bd20a0bd781d5a85ce406a701b556568e5c
HyCraftHD <hycrafthd@live.de>
2019-10-19 14:50:38 +0200

    Add basic method. Buggy

--- src/main/java/info/u_team/attack_speed_enchantment/enchantment/FastAttackSpeedEnchantment.java
+++ src/main/java/info/u_team/attack_speed_enchantment/enchantment/FastAttackSpeedEnchantment.java
@@ -1,14 +1,24 @@
 package info.u_team.attack_speed_enchantment.enchantment;
 
+import java.util.UUID;
+
 import info.u_team.attack_speed_enchantment.AttackSpeedEnchantmentMod;
+import info.u_team.attack_speed_enchantment.init.AttackSpeedEnchantmentEnchantments;
 import info.u_team.u_team_core.enchantment.UEnchantment;
-import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.*;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.AttributeModifier.Operation;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = AttackSpeedEnchantmentMod.MODID)
 public class FastAttackSpeedEnchantment extends UEnchantment {
+	
+	private static final UUID UUID = new UUID(-2801189623500288575L, -7835931777708963832L);
 	
 	public FastAttackSpeedEnchantment(String name) {
 		super(name, Rarity.COMMON, EnchantmentType.WEAPON, new EquipmentSlotType[] { EquipmentSlotType.MAINHAND });
@@ -20,7 +30,16 @@ public class FastAttackSpeedEnchantment extends UEnchantment {
 	}
 	
 	// Handle event
+	@SubscribeEvent
 	public static void apply(LivingEquipmentChangeEvent event) {
-		
+		if (event.getSlot() != EquipmentSlotType.MAINHAND) {
+			return;
+		}
+		final ItemStack stack = event.getTo();
+		final int level = EnchantmentHelper.getEnchantmentLevel(AttackSpeedEnchantmentEnchantments.FAST_ATTACK_SPEED, stack);
+		if (level < 1) {
+			return;
+		}
+		stack.addAttributeModifier(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(UUID, "Weapon modifier", 1, Operation.ADDITION), EquipmentSlotType.MAINHAND);
 	}
 }
--- src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentEnchantments.java
+++ src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentEnchantments.java
@@ -5,6 +5,7 @@ import info.u_team.attack_speed_enchantment.enchantment.FastAttackSpeedEnchantme
 import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
@@ -13,6 +14,7 @@ public class AttackSpeedEnchantmentEnchantments {
 	
 	public static final FastAttackSpeedEnchantment FAST_ATTACK_SPEED = new FastAttackSpeedEnchantment("fast_attack_speed");
 	
+	@SubscribeEvent
 	public static void register(Register<Enchantment> event) {
 		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(AttackSpeedEnchantmentMod.MODID, Enchantment.class).forEach(event.getRegistry()::register);
 	}
