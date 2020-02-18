d7ae82f32294884a4c29ad89b82473b8a9bc4f48
HyCraftHD <hycrafthd@live.de>
2020-02-18 02:00:04 +0100

    Add sign checker

--- src/main/java/info/u_team/attack_speed_enchantment/AttackSpeedEnchantmentMod.java
+++ src/main/java/info/u_team/attack_speed_enchantment/AttackSpeedEnchantmentMod.java
@@ -1,5 +1,6 @@
 package info.u_team.attack_speed_enchantment;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(AttackSpeedEnchantmentMod.MODID)
@@ -7,4 +8,7 @@ public class AttackSpeedEnchantmentMod {
 	
 	public static final String MODID = "attackspeedenchantment";
 	
+	public AttackSpeedEnchantmentMod() {
+		JarSignVerifier.checkSigned(MODID);
+	}
 }
