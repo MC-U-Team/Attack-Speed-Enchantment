d0dec8449f23a76ffceb00b1991b722484e83f68
HyCraftHD <hycrafthd@live.de>
2020-06-26 23:18:23 +0200

    Fix the languages provider

--- src/main/java/info/u_team/attack_speed_enchantment/data/provider/AttackSpeedEnchantmentLanguagesProvider.java
+++ src/main/java/info/u_team/attack_speed_enchantment/data/provider/AttackSpeedEnchantmentLanguagesProvider.java
@@ -1,6 +1,7 @@
 package info.u_team.attack_speed_enchantment.data.provider;
 
 import static info.u_team.attack_speed_enchantment.init.AttackSpeedEnchantmentEnchantments.FAST_ATTACK_SPEED;
+
 import info.u_team.u_team_core.data.*;
 
 public class AttackSpeedEnchantmentLanguagesProvider extends CommonLanguagesProvider {
@@ -15,12 +16,12 @@ public class AttackSpeedEnchantmentLanguagesProvider extends CommonLanguagesProv
 		final String tooltip = "tooltip.attackspeedenchantment.faster_attack_speed";
 		
 		// English
-		add(FAST_ATTACK_SPEED, "Faster Attack Speed");
+		addEnchantment(FAST_ATTACK_SPEED, "Faster Attack Speed");
 		add(tooltip, "%s faster Attack Speed");
 		add(tooltip + ".none", "No Attack Speed");
 		
 		// Deutsch
-		add("de_de", FAST_ATTACK_SPEED, "Schnellere Angriffsgeschwindigkeit");
+		addEnchantment("de_de", FAST_ATTACK_SPEED, "Schnellere Angriffsgeschwindigkeit");
 		add("de_de", tooltip, "%s schnellere Angriffsgeschwindigkeit");
 		add("de_de", tooltip + ".none", "Keine Angriffsgeschwindigkeit");
 	}
