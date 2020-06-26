c2eb7bd59ebd5431deaab6521025f26122f7e29c
HyCraftHD <hycrafthd@live.de>
2020-06-26 23:16:52 +0200

    Use the deferred register to register faster_attack_speed enchantment

--- src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentEnchantments.java
+++ src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentEnchantments.java
@@ -5,13 +5,14 @@ import info.u_team.attack_speed_enchantment.enchantment.FasterAttackSpeedEnchant
 import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class AttackSpeedEnchantmentEnchantments {
 	
 	public static final CommonDeferredRegister<Enchantment> ENCHANTMENTS = CommonDeferredRegister.create(ForgeRegistries.ENCHANTMENTS, AttackSpeedEnchantmentMod.MODID);
 	
-	public static final FasterAttackSpeedEnchantment FAST_ATTACK_SPEED = new FasterAttackSpeedEnchantment("faster_attack_speed");
+	public static final RegistryObject<FasterAttackSpeedEnchantment> FAST_ATTACK_SPEED = ENCHANTMENTS.register("faster_attack_speed", FasterAttackSpeedEnchantment::new);
 	
 	public static void register(IEventBus bus) {
 		ENCHANTMENTS.register(bus);
