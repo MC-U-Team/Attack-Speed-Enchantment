06fa6d04bd4bc3d37769fd41d914098e76699cfd
HyCraftHD <hycrafthd@live.de>
2020-06-26 23:15:09 +0200

    Add deferred register to enchantments

--- src/main/java/info/u_team/attack_speed_enchantment/AttackSpeedEnchantmentMod.java
+++ src/main/java/info/u_team/attack_speed_enchantment/AttackSpeedEnchantmentMod.java
@@ -1,5 +1,7 @@
 package info.u_team.attack_speed_enchantment;
 
+import info.u_team.attack_speed_enchantment.init.AttackSpeedEnchantmentEnchantments;
+import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
@@ -10,6 +12,11 @@ public class AttackSpeedEnchantmentMod {
 	
 	public AttackSpeedEnchantmentMod() {
 		JarSignVerifier.checkSigned(MODID);
+		register();
+	}
+	
+	private void register() {
+		BusRegister.registerMod(AttackSpeedEnchantmentEnchantments::register);
 	}
 	
 }
--- src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentEnchantments.java
+++ src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentEnchantments.java
@@ -2,21 +2,19 @@ package info.u_team.attack_speed_enchantment.init;
 
 import info.u_team.attack_speed_enchantment.AttackSpeedEnchantmentMod;
 import info.u_team.attack_speed_enchantment.enchantment.FasterAttackSpeedEnchantment;
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
+import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.ForgeRegistries;
 
-@EventBusSubscriber(modid = AttackSpeedEnchantmentMod.MODID, bus = Bus.MOD)
 public class AttackSpeedEnchantmentEnchantments {
+	
+	public static final CommonDeferredRegister<Enchantment> ENCHANTMENTS = CommonDeferredRegister.create(ForgeRegistries.ENCHANTMENTS, AttackSpeedEnchantmentMod.MODID);
 	
 	public static final FasterAttackSpeedEnchantment FAST_ATTACK_SPEED = new FasterAttackSpeedEnchantment("faster_attack_speed");
 	
-	@SubscribeEvent
-	public static void register(Register<Enchantment> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(AttackSpeedEnchantmentMod.MODID, Enchantment.class).forEach(event.getRegistry()::register);
+	public static void register(IEventBus bus) {
+		ENCHANTMENTS.register(bus);
 	}
 	
 }
