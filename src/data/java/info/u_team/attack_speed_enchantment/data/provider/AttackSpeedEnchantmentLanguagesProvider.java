b8ce88dcbbc5cf11dc104919349a15cdfaa5d6f0
HyCraftHD <hycrafthd@live.de>
2021-04-25 20:54:04 +0200

    Capitalize to match vanilla

--- src/data/java/info/u_team/attack_speed_enchantment/data/provider/AttackSpeedEnchantmentLanguagesProvider.java
+++ src/data/java/info/u_team/attack_speed_enchantment/data/provider/AttackSpeedEnchantmentLanguagesProvider.java
@@ -15,12 +15,12 @@ public class AttackSpeedEnchantmentLanguagesProvider extends CommonLanguagesProv
 	public void addTranslations() {
 		// English
 		addEnchantment(FASTER_ATTACK_SPEED, "Faster Attack Speed");
-		addTooltip("faster_attack_speed", "faster", 0, "%s less attack cooldown");
-		addTooltip("faster_attack_speed", "none", 0, "No attack cooldown");
+		addTooltip("faster_attack_speed", "faster", 0, "%s Less Attack Cooldown");
+		addTooltip("faster_attack_speed", "none", 0, "No Attack Cooldown");
 		
 		// Deutsch
 		addEnchantment("de_de", FASTER_ATTACK_SPEED, "Schnellere Angriffsgeschwindigkeit");
-		addTooltip("de_de", "faster_attack_speed", "faster", 0, "%s weniger Angriffsabklingzeit");
+		addTooltip("de_de", "faster_attack_speed", "faster", 0, "%s Weniger Angriffsabklingzeit");
 		addTooltip("de_de", "faster_attack_speed", "none", 0, "Keine Angriffsabklingzeit");
 	}
 	
