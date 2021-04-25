25ed499d3f7d0a4ebb357f5e5ba7c21ee54d4be1
HyCraftHD <hycrafthd@live.de>
2021-04-25 17:16:46 +0200

    Register handler

--- src/main/java/info/u_team/attack_speed_enchantment/handler/ToolTipEventHandler.java
+++ src/main/java/info/u_team/attack_speed_enchantment/handler/ToolTipEventHandler.java
@@ -1,21 +1,17 @@
 package info.u_team.attack_speed_enchantment.handler;
 
-import info.u_team.attack_speed_enchantment.AttackSpeedEnchantmentMod;
 import info.u_team.attack_speed_enchantment.init.AttackSpeedEnchantmentEnchantments;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.util.text.*;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.eventbus.api.IEventBus;
 
-@EventBusSubscriber(modid = AttackSpeedEnchantmentMod.MODID, value = Dist.CLIENT)
 public class ToolTipEventHandler {
 	
 	private static final String TRANSLATION_BASE = "tooltip.attackspeedenchantment.faster_attack_speed";
 	
-	@SubscribeEvent
-	public static void onItemTooltip(ItemTooltipEvent event) {
+	private static void onItemTooltip(ItemTooltipEvent event) {
 		final int level = EnchantmentHelper.getEnchantmentLevel(AttackSpeedEnchantmentEnchantments.FAST_ATTACK_SPEED.get(), event.getItemStack());
 		if (level < 1) {
 			return;
@@ -25,5 +21,9 @@ public class ToolTipEventHandler {
 		} else {
 			event.getToolTip().add(new TranslationTextComponent(TRANSLATION_BASE, level * 10 + "%"));
 		}
+	}
+	
+	public static void registerForge(IEventBus bus) {
+		bus.addListener(ToolTipEventHandler::onItemTooltip);
 	}
 }
--- src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentClientConstruct.java
+++ src/main/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentClientConstruct.java
@@ -1,14 +1,17 @@
 package info.u_team.attack_speed_enchantment.init;
 
 import info.u_team.attack_speed_enchantment.AttackSpeedEnchantmentMod;
+import info.u_team.attack_speed_enchantment.handler.ToolTipEventHandler;
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.IModConstruct;
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 @Construct(modid = AttackSpeedEnchantmentMod.MODID, client = true)
 public class AttackSpeedEnchantmentClientConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		BusRegister.registerForge(ToolTipEventHandler::registerForge);
 	}
 	
 }
