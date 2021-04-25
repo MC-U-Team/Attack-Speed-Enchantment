29904cba0d9ca5fb759f9ebbb4f832d5ac53015b
HyCraftHD <hycrafthd@live.de>
2021-04-25 17:40:20 +0200

    Update tooltips to better match vanilla

--- src/main/java/info/u_team/attack_speed_enchantment/handler/ToolTipEventHandler.java
+++ src/main/java/info/u_team/attack_speed_enchantment/handler/ToolTipEventHandler.java
@@ -5,13 +5,14 @@ import info.u_team.attack_speed_enchantment.init.AttackSpeedEnchantmentEnchantme
 import info.u_team.u_team_core.util.TooltipCreator;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.util.text.IFormattableTextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public class ToolTipEventHandler {
 	
-	private static final IFormattableTextComponent NONE_COMPONENT = TooltipCreator.create(AttackSpeedEnchantmentMod.MODID, "faster_attack_speed", "none", 0).mergeStyle(TextFormatting.GOLD);
+	private static final IFormattableTextComponent NONE_COMPONENT = new StringTextComponent(" ").appendSibling(TooltipCreator.create(AttackSpeedEnchantmentMod.MODID, "faster_attack_speed", "none", 0).mergeStyle(TextFormatting.DARK_PURPLE));
 	
 	private static void onItemTooltip(ItemTooltipEvent event) {
 		final int level = EnchantmentHelper.getEnchantmentLevel(AttackSpeedEnchantmentEnchantments.FASTER_ATTACK_SPEED.get(), event.getItemStack());
@@ -21,7 +22,7 @@ public class ToolTipEventHandler {
 		if (level >= 10) {
 			event.getToolTip().add(NONE_COMPONENT);
 		} else {
-			event.getToolTip().add(TooltipCreator.create(AttackSpeedEnchantmentMod.MODID, "faster_attack_speed", "faster", 0, level * 10 + "%").mergeStyle(TextFormatting.GREEN));
+			event.getToolTip().add(new StringTextComponent(" ").appendSibling(TooltipCreator.create(AttackSpeedEnchantmentMod.MODID, "faster_attack_speed", "faster", 0, level * 10 + "%").mergeStyle(TextFormatting.DARK_GREEN)));
 		}
 	}
 	
