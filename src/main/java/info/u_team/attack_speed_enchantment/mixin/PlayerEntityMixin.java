fe3c7bef8ea1bcea7f9c18e6a98f2c1e9927ad5c
HyCraftHD <hycrafthd@live.de>
2021-04-25 18:34:22 +0200

    Add player entity mixin

---
+++ src/main/java/info/u_team/attack_speed_enchantment/mixin/PlayerEntityMixin.java
@@ -0,0 +1,10 @@
+package info.u_team.attack_speed_enchantment.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+	
+}
