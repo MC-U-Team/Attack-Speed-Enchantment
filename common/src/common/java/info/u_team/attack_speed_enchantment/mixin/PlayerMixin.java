51ad2519fd3aa910cdcc3dc86ed87cf2f3ef9477
HyCraftHD <hycrafthd@live.de>
2023-06-22 00:39:22 +0200

    Make mixin more compatible with other mixins

--- common/src/common/java/info/u_team/attack_speed_enchantment/mixin/PlayerMixin.java
+++ common/src/common/java/info/u_team/attack_speed_enchantment/mixin/PlayerMixin.java
@@ -5,7 +5,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import info.u_team.attack_speed_enchantment.hook.PlayerAttackSpeedHook;
+import info.u_team.attack_speed_enchantment.init.AttackSpeedEnchantmentEnchantments;
+import info.u_team.attack_speed_enchantment.util.EnchantmentUtil;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 
 @Mixin(Player.class)
@@ -13,8 +15,10 @@ public class PlayerMixin {
 	
 	@Inject(method = "getCurrentItemAttackStrengthDelay", cancellable = true, at = @At("RETURN"))
 	private void getCurrentItemAttackStrengthDelayWithFasterAttackSpeed(CallbackInfoReturnable<Float> callback) {
-		final float newValue = PlayerAttackSpeedHook.hook(callback.getReturnValueF(), (Player) (Object) this);
-		
-		callback.setReturnValue(newValue);
+		final int level = Mth.clamp(EnchantmentUtil.getEnchantmentLevel(AttackSpeedEnchantmentEnchantments.FASTER_ATTACK_SPEED.get(), ((Player) (Object) this).getMainHandItem()), 0, 10);
+		if (level > 0) {
+			System.out.println("LEVEL: " + level);
+			callback.setReturnValue(callback.getReturnValueF() * (1 - (level * 0.1F)));
+		}
 	}
 }
