3dcd2b5a0b923337c4d6039ebc412cda39730ead
HyCraftHD <hycrafthd@live.de>
2023-06-21 16:41:39 +0200

    Add config value for enchantment level. Implement #19

--- src/main/java/info/u_team/attack_speed_enchantment/enchantment/FasterAttackSpeedEnchantment.java
+++ src/main/java/info/u_team/attack_speed_enchantment/enchantment/FasterAttackSpeedEnchantment.java
@@ -1,5 +1,10 @@
 package info.u_team.attack_speed_enchantment.enchantment;
 
+import java.util.function.Supplier;
+
+import com.google.common.base.Suppliers;
+
+import info.u_team.attack_speed_enchantment.config.CommonConfig;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
@@ -8,13 +13,15 @@ import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class FasterAttackSpeedEnchantment extends Enchantment {
 	
+	private final Supplier<Integer> maxLevel = Suppliers.memoize(() -> CommonConfig.getInstance().maxEnchantmentLevel.get());
+	
 	public FasterAttackSpeedEnchantment() {
 		super(Rarity.COMMON, EnchantmentCategory.WEAPON, new EquipmentSlot[] { EquipmentSlot.MAINHAND });
 	}
 	
 	@Override
 	public int getMaxLevel() {
-		return 10;
+		return maxLevel.get();
 	}
 	
 	@Override
