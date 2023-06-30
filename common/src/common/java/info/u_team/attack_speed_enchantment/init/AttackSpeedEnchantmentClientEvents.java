954b4b62e37a8e44c3a11c532e3ad89bbc0c2a9d
HyCraftHD <hycrafthd@live.de>
2023-06-30 12:08:57 +0200

    Use event api from uteamcore for tooltip

--- common/src/common/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentClientEvents.java
+++ common/src/common/java/info/u_team/attack_speed_enchantment/init/AttackSpeedEnchantmentClientEvents.java
@@ -1,21 +1,22 @@
-package info.u_team.attack_speed_enchantment.handler;
+package info.u_team.attack_speed_enchantment.init;
 
 import java.util.List;
 
 import info.u_team.attack_speed_enchantment.AttackSpeedEnchantmentReference;
-import info.u_team.attack_speed_enchantment.init.AttackSpeedEnchantmentEnchantments;
+import info.u_team.u_team_core.api.event.ClientEvents;
 import info.u_team.u_team_core.util.EnchantmentUtil;
 import info.u_team.u_team_core.util.TooltipCreator;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 
-public class TooltipEventHandler {
+public class AttackSpeedEnchantmentClientEvents {
 	
 	private static final MutableComponent NONE_COMPONENT = Component.literal(" ").append(TooltipCreator.create(AttackSpeedEnchantmentReference.MODID, "faster_attack_speed", "none", 0).withStyle(ChatFormatting.DARK_PURPLE));
 	
-	public static void onItemTooltip(ItemStack stack, List<Component> lines) {
+	private static void onItemTooltip(ItemStack stack, TooltipFlag flag, List<Component> lines) {
 		final int level = EnchantmentUtil.getEnchantmentLevel(AttackSpeedEnchantmentEnchantments.FASTER_ATTACK_SPEED.get(), stack);
 		if (level < 1) {
 			return;
@@ -26,4 +27,9 @@ public class TooltipEventHandler {
 			lines.add(Component.literal(" ").append(TooltipCreator.create(AttackSpeedEnchantmentReference.MODID, "faster_attack_speed", "faster", 0, level * 10 + "%").withStyle(ChatFormatting.DARK_GREEN)));
 		}
 	}
+	
+	static void register() {
+		ClientEvents.registerItemTooltip(AttackSpeedEnchantmentClientEvents::onItemTooltip);
+	}
+	
 }
